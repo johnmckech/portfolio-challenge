c6281690f0ffd80c8a920e075fe14a87dfad4146
John McK <john.mckech3@gmail.com>
2020-10-19 17:38:59 -0500

    scaffolding added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React from 'react';
+import Header from './components/Header';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src= "screenshot.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          John <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <Link
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          to="/"
         >
-          Learn React
-        </a>
+         Home 
+        </Link>
+        <Link
+          className="App-link"
+          to="/about"
+        >
+         About 
+        </Link>
       </header>
+      <Switch>
+        <Route exact={true} path="/">
+          <Header name='john'/>
+        </Route>
+        <Route path="/about">
+          <div>
+            <h2>about me</h2>
+            <p>I'm John and I like sports</p>
+          <Header name='michael'/></div>
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
